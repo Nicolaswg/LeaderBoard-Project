f0b8b23556d7689e27a07950f5fad64292a1ce25
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-01-25 08:37:32 -0400

    Add linter file structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool:'inline-source-map',
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
